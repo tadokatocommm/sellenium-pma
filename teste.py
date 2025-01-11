ad4cf811705b19d8d543900e51a3e688c6f8f152
tadokatocommm <tadokatocom@gmail.com>
2025-01-11 10:08:05 -0300

    2st fix

--- teste.py
+++ teste.py
@@ -3,15 +3,15 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import time 
 
-# Caminho para o perfil do Chrome
+# Pegando o caminho para o perfil do Chrome onde tem login salvo
 profile_path = r'C:\Users\LuanPC\AppData\Local\Google\Chrome\User Data'
 
-# Configurações do Chrome
+# Configurações do Chrome para usar o login existente
 chrome_options = Options()
 chrome_options.add_argument(f"user-data-dir={profile_path}")
 chrome_options.add_argument("profile-directory=Default")  
 
-# Caminho do ChromeDriver
+# Caminho do ChromeDriver (Automação)
 service = Service(r'C:\Users\LuanPC\Documents\pma-script\chromedriver.exe')
 
 # Inicializar o navegador com o serviço
