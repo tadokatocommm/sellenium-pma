8d3a533c9ee27af57d6753f3655f6269c694f9de
Luan Santos Vieira <lsvieira2@pma.rima>
2025-01-17 10:26:23 -0300

    fix branch

--- teste.py
+++ teste.py
@@ -1,20 +1,26 @@
+import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-import time 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
 
-# Pegando o caminho para o perfil do Chrome onde tem login salvo
-profile_path = r'C:\Users\LuanPC\AppData\Local\Google\Chrome\User Data'
+# Carregando os e-mails do Excel
+excel_path = 'dados.xlsx'
+df = pd.read_excel(excel_path)
+first_email = df['Email'].iloc[0]  # Pegando o primeiro e-mail da coluna
 
-# Configurações do Chrome para usar o login existente
-chrome_options = Options()
-chrome_options.add_argument(f"user-data-dir={profile_path}")
-chrome_options.add_argument("profile-directory=Default")  
-
-# Caminho do ChromeDriver (Automação)
-service = Service(r'C:\Users\LuanPC\Documents\pma-script\chromedriver.exe')
-
-# Inicializar o navegador com o serviço
-driver = webdriver.Chrome(service=service, options=chrome_options)
-driver.get('https://www.instagram.com/')
+# Configurando o Selenium
+driver = webdriver.Chrome()
+driver.get('https://www.google.com.br/?hl=pt-BR')
 time.sleep(5)
+
+# Localizando o campo de pesquisa do Google e inserindo o e-mail
+search_box = driver.find_element(By.NAME, 'q')  # 'q' é o nome do campo de busca do Google
+search_box.send_keys(first_email)  # Inserindo o e-mail no campo de busca
+search_box.send_keys(Keys.RETURN)  # Pressionando Enter para pesquisar
+
+# Aguarde alguns segundos para ver o resultado
+time.sleep(5)
+
+# Fechar o navegador (opcional)
+driver.quit()
